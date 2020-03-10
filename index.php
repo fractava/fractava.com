e86d7fb7238285f049c5abdb31d17d3c3c8d7697
FRACTAVA <mail@fractava.com>
2020-03-10 15:58:11 +0000

    moved jQuery from cdn to local file

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <head>
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112803937-1"></script>
+    
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 	<link rel="shortcut icon" type="image/png" href="/assets/img/logo/logo_black.png"/>
+	
 	<link rel="stylesheet" href="/css/style.css">
 	<link rel="stylesheet" href="/css/fonts.css">
 	<link rel="stylesheet" href="/css/cookieAlert.css">
-	<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
+	
+	<script src='/js/libaries/jquery.min.js'></script>
+	
 	<script src='/js/main.js'></script>
 	<script src='/js/sectionNavigation.js'></script>
 	<script src='/js/functions.js'></script>

813b1f20e2999be77b33b6de7254ac43a4425643
FRACTAVA <mail@fractava.com>
2020-04-09 17:24:29 +0000

    added sccs/sass support

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   module: {
     rules: [
       {test: /\.vue$/, loader: 'vue-loader'},
-      {test: /\.css$/,use: ['vue-style-loader', 'css-loader']},
+      {test: /\.css$/,use: ['vue-style-loader', 'css-loader', 'sass-loader']},
       {test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader']},
       {test: /\.(png|svg|jpg|gif)$/, use: {"loader": 'file-loader', options: {esModule: false}}},
     ]

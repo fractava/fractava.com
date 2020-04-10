740b5754626dc10d4a538187c520e9cd03963d2f
Jonathan Treffler <trefflerj@web.de>
2020-04-10 08:10:56 +0000

    Delete config.inc.php to resolve merge conflict

--- account/inc/config.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-$db_host = 'localhost';
-$db_name = 'fractemtron';
-$db_user = 'fractemtron';
-$db_password = '#WeLoveMySQL';
-$pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_password);

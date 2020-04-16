638f54ce94bdb012a4712470866a4e95dda11935
Jonathan Treffler <trefflerj@web.de>
2020-04-16 12:36:25 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,23 +1,66 @@
 #!/bin/bash
 
-if [ ! -f /.notfirststart ]; then
-    echo "first Start"
+check-if-first-start
+{
+    if [ ! -f /.notfirststart ]; then
+        echo "first Start"
+        first-start
+        echo "first start init done"
+    fi
+}
 
-    rm -f -R /config/*
+first-start
+{
+    if [ ! -f /config ]; then
+        backup-config
+    fi
+
+    clear-config-folder
+
     mv /config-buildtime/* /config/
-
     cp /config/nginx/custom-config-include.conf /etc/nginx/nginx.conf
 
+    if [ ! -f /config-backup/ ]; then
+        copy-persistent-files-back
+    fi
+    
     touch /.notfirststart
+}
 
-    echo "first start init done"
-fi
+clear-config-folder
+{
+    rm -f -R /config/*
+}
 
-echo "starting nginx ..."
-nginx
-echo "nginx start done"
+backup-config()
+{
+    echo "config backup"
+    mkdir /config-backup/
+    cp /config/* /config-backup/
+    ls /config-backup/
+}
 
-while true
-do
-    sleep 1
-done
+copy-persistent-files-back()
+{
+    echo "copy-persistent-files-back"
+    cp /config-backup/www/js/modules/config.js /config/www/js/modules/
+}
+
+start-nginx()
+{
+    echo "starting nginx ..."
+    nginx
+    echo "nginx start done"
+}
+
+wait-forever()
+{
+    while true
+    do
+        sleep 1
+    done
+}
+
+check-if-first-start
+start-nginx
+wait-forever
